01a0627d9f7de2077b5d022f1bacedeb05a51e83
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-03-31 11:02:48 -0500

    changed all use of event.keyCode to event.key because keyCode is depricated

--- src/constants/handleKeyDownLogic/handleKeyDownLogic.js
+++ src/constants/handleKeyDownLogic/handleKeyDownLogic.js
@@ -1,50 +1,51 @@
 export const handleKeyDown = (dispatch, event) => {
-  if (event.keyCode >= 48 && event.keyCode <= 57 && event.shiftKey === false) {
+  if (
+    parseInt(event.key) >= 0 &&
+    parseInt(event.key) <= 9 &&
+    event.shiftKey === false
+  ) {
     const value = event.key;
     dispatch('BUILD_OPERAND', value);
   }
 
-  if (event.keyCode >= 96 && event.keyCode <= 105) {
-    const value = event.key;
-    dispatch('BUILD_OPERAND', value);
-  }
-
-  if (event.keyCode === 8) {
+  if (event.key === 'Backspace') {
     dispatch('BACKSPACE');
   }
 
-  if (
-    event.keyCode === 13 ||
-    (event.keyCode === 187 && event.shiftKey === false)
-  ) {
+  if (event.key === 'Enter') {
+    // console.log('ENTER');
     dispatch('EQUALS');
   }
 
-  if (event.keyCode === 27) {
+  if (event.key === '=' && event.shiftKey === false) {
+    dispatch('EQUALS');
+  }
+
+  if (event.key === 'Escape') {
     dispatch('CLEAR_FUNCTIONS', 'C');
   }
 
-  if (event.keyCode === 187 && event.shiftKey === true) {
+  if (event.key === '+' && event.shiftKey === true) {
     dispatch('SUM_SUB_MULT_DIV', '+');
   }
 
-  if (event.keyCode === 189) {
+  if (event.key === '-') {
     dispatch('SUM_SUB_MULT_DIV', '-');
   }
 
-  if (event.keyCode === 56 && event.shiftKey === true) {
+  if (event.key === '*' && event.shiftKey === true) {
     dispatch('SUM_SUB_MULT_DIV', 'x');
   }
 
-  if (event.keyCode === 191) {
+  if (event.key === '/') {
     dispatch('SUM_SUB_MULT_DIV', '/');
   }
 
-  if (event.keyCode === 53 && event.shiftKey === true) {
+  if (event.key === '%' && event.shiftKey === true) {
     dispatch('PERCENT');
   }
 
-  if (event.keyCode === 190) {
+  if (event.key === '.') {
     dispatch('DECIMAL');
   }
 };
